cbb408b7ccb8e41e0a28dd401658e481a54acd73
Ashmin Jayson <ashminjayson10@gmail.com>
2022-12-31 20:23:43 +0530

    Added Login Page and Verification

--- main.py
+++ main.py
@@ -1,66 +1,54 @@
 import mysql.connector
+from flask import Flask, request, render_template
 import time
-from flask import Flask, render_template, request
-
-class dbConnection:
-    def __init__(self):
-
-        self.conn = mysql.connector.connect(        
-        host = "localhost",
-        username = "root",
-        password = "DAMN*you4",
-        database = "bankingsystem",
-        )
-
-        self.curr = self.conn.cursor()
-
-        print('Connection to db has been established successfully')
 
 
-    def verifyLogin(self, accno, password):
-        execStatement = "select * from user"
-        self.curr.execute(execStatement)
-        # self.curr.execute(execStatement, [int(accno)])
-        validPassword = self.curr.fetchall()
-        return validPassword
+try :
+    conn = mysql.connector.connect(
+            host = "localhost",
+            username = "bankadmin",
+            password = "bankadmin",
+            database = "bankingsystem",
+    )
 
-    def log(self):
-        execStatement = "select * from user"
-        self.curr.execute(execStatement)
-        # self.curr.execute(execStatement, [int(accno)])
-        validPassword = self.curr.fetchall()
-        return validPassword
+    curr = conn.cursor()
 
-    def __del__(self):
-        self.curr.close()
-        self.conn.close()
-        print('Connection to db has been terminated successfully')
+    print('Connection to DB has been established')
+except:
+    print('Connection to the DB has failed')
 
 
 
-app = Flask(__name__)
+
+app = Flask('__name__')
+
+
+authtoken = 0
+
 @app.route("/")
-def hello():
-    dbObj = dbConnection()
-    users = dbObj.log()
-    print(users)
-    return render_template('login.html', users = users)
-
-@app.route("/login", methods = ['POST', 'GET'])
 def login():
+    global authtoken
+    authtoken = 0
+    return render_template('login.html', valid='')
+
+@app.route("/", methods = ['POST','GET'])
+def loginverify():
+
+    global authtoken
     if request.method == 'POST':
         accno = request.form["accno"]
         password = request.form["password"]
-        dbObj = dbConnection()
-        state = dbObj.verifyLogin(accno,password)
+        curr.execute('select passwordCheck(%s,%s)', [int(accno), password])
+        validity = curr.fetchone()
+        if validity[0] == 1:
+            authtoken = 1
 
-    if state == True:
-        return 'Successful'
+    if authtoken == 0:
+        return render_template('login.html', valid = 'Please re-check the credentials')
     else:
-        return 'Unsuccesful'
+        authtoken = 0
+        return 'happy banking'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
-
-
